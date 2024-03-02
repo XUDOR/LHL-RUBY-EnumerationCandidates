b7974217ff32946411abe1c8a272388f47c2c6ec
Roderick Shoolbraid <rdxenv@gmail.com>
2024-03-01 18:46:50 -0800

    tasks filters 1-4

--- filters.rb
+++ filters.rb
@@ -1,16 +1,23 @@
-# In this file we define the methods to help filter out candidates
-# This way, we keep these methods separated from other potential parts of the program
+require 'date' # Ensure you have this require at the top if you're using Date
+
+def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
+end
 
 def find(id)
-    # Your code Here
+  @candidates.find { |candidate| candidate[:id] == id }
+end
+
+def qualified_candidates(candidates)
+  candidates.select do |candidate|
+    experienced?(candidate) &&
+    candidate[:github_points] >= 100 &&
+    (candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')) &&
+    (Date.today - candidate[:date_applied]).to_i <= 15 &&
+    candidate[:age] > 17
   end
-  
-  def experienced?(candidate)
-    # Your code Here
-  end
-  
-  def qualified_candidates(candidates)
-    # Your code Here
-  end
-  
-  # More methods will go below
+end
+
+def ordered_by_qualifications(candidates)
+  candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+end
